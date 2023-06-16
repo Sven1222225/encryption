cd62fd7f12e4d8339706d70429b08e3a4cec8207
Sven Weber <sven@sven1222225.ch>
2023-06-16 14:50:57 +0200

    basic js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-console.log('etst')
+document.querySelectorAll('.btn-crypt').forEach(function(btn) {btn.addEventListener('click', startCrypt)});
+let methodSelectorElement = document.querySelector('#encryption');
+
+function startCrypt(){
+    console.log(methodSelectorElement.value);
+}
