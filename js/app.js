4469057b0f29d15db06aa5f54f0f3ce6f6016709
Sven Weber <sven@sven1222225.ch>
2023-06-23 14:13:24 +0200

    added xor

--- js/app.js
+++ js/app.js
@@ -3,4 +3,23 @@ let methodSelectorElement = document.querySelector('#encryption');
 
 function startCrypt(){
     console.log(methodSelectorElement.value);
+
+    let cipher = document.querySelector('#cipher').value;
+
+    let out = encodeXOR(document.querySelector('#input').value, cipher)
+
+    console.log(out);
+    document.querySelector('#output').value = out;
+}
+
+/**
+ * xor should be one character long
+ * @param {String} input 
+ * @param {String} xor 
+ * @returns {String}
+ */
+function encodeXOR(input, xor){
+    return input.split('').map(function(e){
+        return String.fromCharCode(e.charCodeAt(0) ^ xor.charCodeAt(0))
+    }).join('');
 }
