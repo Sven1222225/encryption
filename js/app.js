f380b071965531ea1184513a2737ad0544ceb759
Sven Weber <sven@sven1222225.ch>
2023-06-30 14:29:05 +0200

    fixed caesar

--- js/app.js
+++ js/app.js
@@ -1,125 +1,79 @@
 // Element
-const inputElement = document.getElementById('input');
-const outputElement = document.getElementById('output');
+const navCaesar = document.getElementById('nav_caesar').addEventListener('click', handleNav);
+const navXor = document.getElementById('nav_xor').addEventListener('click', handleNav);
 
-const selectorElement = document.getElementById('methodSelector');
-const checkboxElement = document.getElementById('encDec');
-const submitElement = document.getElementById('button');
-const cipherElement = document.getElementById('cipher');
+const caesarButtonEncrypt = document.getElementById('caesar_button_encrypt').addEventListener('click', caesarEncrypt);
+const caesarButtonDecrypt = document.getElementById('caesar_button_decrypt').addEventListener('click', caesarDecrypt);
 
-// Variable
-let isDecrypt = false;
+//Handle Navigation
+function handleNav() {
+    const navs = document.querySelectorAll('[id^="nav_"]');
+    const sections = document.querySelectorAll('[id^="section_"]');
 
-// Event Listeners
+    navs.forEach(function(nav){
+        nav.dataset.state = 'unselected';
+    });
 
-// Checkbox Listener
-checkboxElement.addEventListener('click', function() {
-  if (checkboxElement.checked) {
-    submitElement.innerHTML = "Entschlüsseln";
-    isDecrypt = true;
-  } else {
-    submitElement.innerHTML = "Verschlüsseln";
-    isDecrypt = false;
-  }
-});
+    this.dataset.state = 'selected';
 
-// Submit Listener
-submitElement.addEventListener('click', function() {
-  if (isDecrypt) {
-    decrypt();
-  } else {
-    encrypt();
-  }
-});
+    sections.forEach(function(section){
+        section.classList.add('hidden');
+    });
 
-selectorElement.addEventListener('change', function() {
-  // You can add code here if needed
-});
-
-// Encryption Function
-function encrypt() {
-  let result = "";
-  let method = selectorElement.value;
-  let text = inputElement.value;
-  let cipher = cipherElement.value;
-
-  switch (method) {
-    case "caesar":
-      result = caesarEncrypt(text, cipher);
-      break;
-    case "xor":
-      result = XORencode(text, cipher);
-      break;
-
-    default:
-      result = "Error";
-  }
-
-  // Output
-  outputElement.value = result;
-}
-
-// Decryption Function
-function decrypt() {
-  let result = "";
-  let method = selectorElement.value;
-  let text = inputElement.value;
-  let cipher = cipherElement.value;
-
-  switch (method) {
-    case "caesar":
-      result = caesarDecrypt(text, cipher);
-      break;
-    case "xor":
-      result = XORencode(text, cipher);
-      break;
-
-    default:
-      result = "Error";
-  }
-
-  // Output
-  outputElement.value = result;
+    const navName = this.id.replace('nav', 'section');
+    
+    selectedSection = document.getElementById(navName);
+    selectedSection.classList.remove('hidden');
 }
 
 // Caesar Encryption
-function caesarEncrypt(input, cipher) {
-  let shift = parseInt(cipher);
-  let output = "";
+function caesarEncrypt() {
+    const inputElement = document.getElementById('caesar_input_encrypt');
+    const outputElement = document.getElementById('caesar_output_encrypt');
+    const cipherElement = document.getElementById('caesar_cipher_encrypt');
 
-  for (let i = 0; i < input.length; i++) {
-    let c = input.charCodeAt(i);
+    let shift = parseInt(cipherElement.value);
+    let input = inputElement.value;
+    let output = "";
 
-    if (c >= 65 && c <= 90) {
-      output += String.fromCharCode((c - 65 + shift) % 26 + 65);
-    } else if (c >= 97 && c <= 122) {
-      output += String.fromCharCode((c - 97 + shift) % 26 + 97);
-    } else {
-      output += input.charAt(i);
+    for (let i = 0; i < input.length; i++) {
+        let c = input.charCodeAt(i);
+
+        if (c >= 65 && c <= 90) {
+            output += String.fromCharCode((c - 65 + shift) % 26 + 65);
+        } else if (c >= 97 && c <= 122) {
+            output += String.fromCharCode((c - 97 + shift) % 26 + 97);
+        } else {
+            output += input.charAt(i);
+        }
     }
-  }
 
-  return output;
+    outputElement.value = output;
 }
 
 // Caesar Decryption
-function caesarDecrypt(text, cipher) {
-  let shift = parseInt(cipher);
-  let result = "";
+function caesarDecrypt() {
+    const inputElement = document.getElementById('caesar_input_decrypt');
+    const outputElement = document.getElementById('caesar_output_decrypt');
+    const cipherElement = document.getElementById('caesar_cipher_decrypt');
 
-  for (let i = 0; i < text.length; i++) {
-    let c = text.charCodeAt(i);
+    let shift = parseInt(cipherElement.value);
+    let text = inputElement.value;
+    let result = "";
 
-    if (c >= 65 && c <= 90) {
-      result += String.fromCharCode((c - 65 - shift + 26) % 26 + 65);
-    } else if (c >= 97 && c <= 122) {
-      result += String.fromCharCode((c - 97 - shift + 26) % 26 + 97);
-    } else {
-      result += text.charAt(i);
+    for (let i = 0; i < text.length; i++) {
+        let c = text.charCodeAt(i);
+
+        if (c >= 65 && c <= 90) {
+            result += String.fromCharCode((c - 65 - shift + 26) % 26 + 65);
+        } else if (c >= 97 && c <= 122) {
+            result += String.fromCharCode((c - 97 - shift + 26) % 26 + 97);
+        } else {
+            result += text.charAt(i);
+        }
     }
-  }
 
-  return result;
+    outputElement.value = result;
 }
 
 /**
@@ -128,8 +82,8 @@ function caesarDecrypt(text, cipher) {
  * @param {String} xor 
  * @returns {String}
  */
-function XORencode(input, xor){
-    return input.split('').map(function(e){
+function XORencode(input, xor) {
+    return input.split('').map(function (e) {
         return String.fromCharCode(e.charCodeAt(0) ^ xor.charCodeAt(0))
     }).join('');
 }
