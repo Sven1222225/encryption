c2051f4bd1367953388aef8ee521147eed661277
Fightingpainter <122602731+Fightingpainter@users.noreply.github.com>
2023-07-01 14:27:40 +0200

    Atbash idk

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Element
 const navCaesar = document.getElementById('nav_caesar').addEventListener('click', handleNav);
 const navXor = document.getElementById('nav_xor').addEventListener('click', handleNav);
+const navAtbash = document.getElementById('nav_atbash').addEventListener('click', handleNav);
 
 const caesarButtonEncrypt = document.getElementById('caesar_button_encrypt').addEventListener('click', caesarEncrypt);
 const caesarButtonDecrypt = document.getElementById('caesar_button_decrypt').addEventListener('click', caesarDecrypt);
 const xorButtonEncrypt = document.getElementById('xor_button_encrypt').addEventListener('click', xorEncrypt);
 const xorButtonDecrypt = document.getElementById('xor_button_decrypt').addEventListener('click', xorDecrypt);
+const atbashButtonEncrypt = document.getElementById('atbash_button_encrypt').addEventListener('click', atbashEncrypt);
+const atbashButtonDecrypt = document.getElementById('atbash_button_decrypt').addEventListener('click', atbashDecrypt);
 
 //Handle Navigation
 function handleNav() {
@@ -98,4 +101,56 @@ function xorDecrypt() {
     outputElement.value = input.split('').map(function (e) {
         return String.fromCharCode(e.charCodeAt(0) ^ cipher.charCodeAt(0))
     }).join('');
+}
+
+// Atbash Encryption
+function atbashEncrypt() {
+    const inputElement = document.getElementById('atbash_input_encrypt');
+    const outputElement = document.getElementById('atbash_output_encrypt');
+    
+    let input = inputElement.value;
+    let output = "";
+
+    for (let i = 0; i < input.length; i++) {
+        let c = input.charCodeAt(i);
+
+        if (c >= 65 && c <= 90) {
+            // Uppercase letter
+            output += String.fromCharCode(90 - (c - 65));
+        } else if (c >= 97 && c <= 122) {
+            // Lowercase letter
+            output += String.fromCharCode(122 - (c - 97));
+        } else {
+            // Non-alphabetic character, copy as is
+            output += input.charAt(i);
+        }
+    }
+
+    outputElement.value = output;
+}
+
+// Atbash Decryption
+function atbashDecrypt() {
+    const inputElement = document.getElementById('atbash_input_decrypt');
+    const outputElement = document.getElementById('atbash_output_decrypt');
+    
+    let input = inputElement.value;
+    let output = "";
+
+    for (let i = 0; i < input.length; i++) {
+        let c = input.charCodeAt(i);
+
+        if (c >= 65 && c <= 90) {
+            // Uppercase letter
+            output += String.fromCharCode(90 - (c - 65));
+        } else if (c >= 97 && c <= 122) {
+            // Lowercase letter
+            output += String.fromCharCode(122 - (c - 97));
+        } else {
+            // Non-alphabetic character, copy as is
+            output += input.charAt(i);
+        }
+    }
+
+    outputElement.value = output;
 }
